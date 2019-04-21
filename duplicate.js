11a918cc813fdb7881a35ce86c68e6d5ba4b3306
CristinaBirsan <49725536+CristinaBirsan@users.noreply.github.com>
2019-04-21 13:39:08 +0300

    upload good version for problem 3

--- duplicate.js
+++ duplicate.js
@@ -1,24 +1,34 @@
 function duplicate(arr){
-var a =new Set(arr);
-if (arr.length=== a.size)
-  {
-    return "Nu sunt duplicate";
-  }else return "Sunt duplicate";
+  var a =new Set(arr);
+  if (arr.length === a.size){
+     return false;
+  }
+  else {
+      return true;
+  }
 }
-
 
 var unitTest={};
 
 unitTest.testDupl = function(method){
-  var arr=[15,20,43,19,42,20,15];
-   var a =new Set(arr);
-  var result = arr.length === a.size;
-  
-  if (method(arr) === result){
+  var arr=[15,43,19,42,20,20];
+  var result
+  var a =new Set(arr);
+  if(arr.length === a.size)
+  {
+   result=false;
+  }
+  else {
+      return true;
+  }
+
+  var test=method(arr);
+
+  if (test ===false && result==false){
     return false;
   }
   else{
     return true;
   }
 };
-console.log(unitTest.testDupl(duplicate));
+unitTest.testDupl(duplicate);
